6c152bcb25d46f57564fefd591a7ab13163d2dd4
Antoine Corcy <dev@sbin.dk>
2015-01-13 23:04:00 +0100

    add author to classes

--- src/Command/Expand.php
+++ src/Command/Expand.php
@@ -17,6 +17,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
 class Expand extends Command
 {
     /**
